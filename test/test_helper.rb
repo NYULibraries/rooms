693bc909969969db953f9f5c8f9f3cb2c522d1fd
B Alter <barnaby.alter@gmail.com>
2013-02-26 16:56:51 -0500

    Rcov for test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,10 @@
+unless ENV['CI']
+  require 'simplecov'
+  require 'simplecov-rcov'
+  SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+  SimpleCov.start
+end
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'

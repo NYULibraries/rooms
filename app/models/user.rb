d74c980359c7138ecc61057c94996d37ff45ce93
B Alter <barnaby.alter@gmail.com>
2013-06-26 13:59:16 -0400

    Added scopes and cleanup rake

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   has_many :reservations, :dependent => :destroy
   
   attr_accessible :crypted_password, :current_login_at, :current_login_ip, :email, :firstname, :last_login_at, :last_login_ip, :last_request_at, :lastname, :login_count, :mobile_phone, :password_salt, :persistence_token, :refreshed_at, :session_id, :user_attributes, :username
+  scope :non_admin, where("user_attributes NOT LIKE '%:room_reserve_admin: true%'")
+  scope :admin, where("user_attributes LIKE '%:room_reserve_admin: true%'")
+  scope :inactive, where("last_request_at < ?", 1.year.ago)
 
   serialize :user_attributes
   
--- lib/tasks/cleanup.rake
+++ lib/tasks/cleanup.rake
@@ -3,7 +3,7 @@ namespace :cleanup do
     desc "Cleanup users who have been inactive for over a year"
     task :users => :environment do
       @log = Logger.new("log/destroy_inactive_users.log")
-      destroyed = User.destroy_all(["last_request_at < ?", 1.year.ago])
+      destroyed = User.non_admin.inactive.destroy_all
       @log.error "[#{Time.now.to_formatted_s(:db)}] #{destroyed.count} users destroyed"
     end
     

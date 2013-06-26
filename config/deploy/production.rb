841fc448360322a6dc760ca08a8e203c4d1662fb
B Alter <barnaby.alter@gmail.com>
2013-06-26 11:47:29 -0400

    Updated submodule with multiple files and changed deploy script

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,2 @@
 set :rails_env, "production"
-# Vars loaded via rails_config
-(set :app_settings, eval(run_locally("rails runner -e #{rails_env} 'puts \"{ :scm_username => \" + Settings.capistrano.scm_username.inspect + \", :app_path => \" + Settings.capistrano.path.inspect + \", :user => \" + Settings.capistrano.user.inspect + \", :server1 => \" + Settings.capistrano.server1.inspect + \", :server2 => \" + Settings.capistrano.server2.inspect + \" } \"'")))
-set :rvm_type, :user
-set :scm_username, app_settings[:scm_username]
-set :app_path, app_settings[:app_path]
-set :user, app_settings[:user]
-set :server1, app_settings[:server1]
-set :server2, app_settings[:server2]
-server "#{server1}", :app, :web, :db, :primary => true
-server "#{server2}", :app, :web, :db
-set :deploy_to, "#{app_path}#{application}"
 set :branch, "master"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,11 +1,2 @@
 set :rails_env, "staging"
-# Vars loaded via rails_config
-(set :app_settings, eval(run_locally("rails runner -e #{rails_env} 'puts \"{ :scm_username => \" + Settings.capistrano.scm_username.inspect + \", :app_path => \" + Settings.capistrano.path.inspect + \", :user => \" + Settings.capistrano.user.inspect + \", :server => \" + Settings.capistrano.server.inspect + \" } \"'")))
-set :rvm_type, :user
-set :scm_username, app_settings[:scm_username]
-set :app_path, app_settings[:app_path]
-set :user, app_settings[:user]
-set :host, app_settings[:server]
-server "#{host}", :app, :web, :db, :primary => true
-set :deploy_to, "#{app_path}#{application}"
-#set :branch, "development"
+set :branch, "master"

a2e409cb30c62046959d2955432c3533f169645a
B Alter <barnaby.alter@gmail.com>
2013-10-04 16:30:38 -0400

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130226165700) do
+ActiveRecord::Schema.define(:version => 20131003170025) do
 
   create_table "reservations", :force => true do |t|
     t.integer  "user_id"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(:version => 20130226165700) do
     t.datetime "last_login_at"
     t.text     "user_attributes"
     t.datetime "refreshed_at"
+    t.integer  "roles_mask"
   end
 
 end

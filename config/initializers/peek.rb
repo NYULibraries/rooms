7032bf8ca8ddd28ce5a79d4c834ea404c4602b24
B Alter <barnaby.alter@gmail.com>
2013-11-12 16:55:58 -0500

    Fixed peek settings:

--- config/initializers/peek.rb
+++ config/initializers/peek.rb
@@ -1,4 +1,4 @@
-Peek.into Peek::Views::Git, :nwo => 'github/janky'
+Peek.into Peek::Views::Git, :nwo => 'NYULibraries/rooms'
 Peek.into Peek::Views::Mysql2
 Peek.into Peek::Views::Dalli
 Peek.into Peek::Views::PerformanceBar

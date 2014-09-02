8ebe128c1522533b78ca181334958c152cc9b3e9
B Alter <barnaby.alter@gmail.com>
2014-09-02 16:17:12 -0400

    Removed Peek

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rooms::Application.routes.draw do
-  mount Peek::Railtie => '/peek'
 
   scope "admin" do
     get 'rooms/sort' => "rooms#index_sort", :as => "sort_rooms"
@@ -7,27 +6,27 @@ Rooms::Application.routes.draw do
     match 'blocks/destroy/:id' => "blocks#destroy", :as => "destroy_block"
     match 'blocks/destroy_existing_reservations' => "blocks#destroy_existing_reservations", :as => 'destroy_existing_reservations'
     match 'blocks/index_existing_reservations' => "blocks#index_existing_reservations", :as => 'index_existing_reservations'
-    
+
     resources :user_sessions
     resources :users
     resources :rooms
-    resources :blocks 
+    resources :blocks
     resources :reports
     resources :room_groups
-   
+
     match 'reports' => "reservations#generate_report"
   end
   get 'rooms/:id' => "rooms#show", :as => "show_room_details"
-  
+
   match 'reservations/resend_email' => "reservations#resend_email"
   resources :reservations do
     put "delete" => "reservations#delete"
   end
-  
+
   match 'login', :to => 'user_sessions#new', :as => :login
   match 'logout', :to => 'user_sessions#destroy', :as => :logout
   match 'validate', :to => 'user_sessions#validate', :as => :validate
-  
+
   match 'admin' => "rooms#index"
 
   root :to => "reservations#index"

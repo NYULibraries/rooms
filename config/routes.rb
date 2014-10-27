01c57c5c7edb36c6f512ceefb5978a0711a674ed
hab278 <hab278@nyu.edu>
2014-10-27 11:14:05 -0400

    Set up routes for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,38 @@
 Rooms::Application.routes.draw do
 
-  namespace :api, defaults: {format: 'json'} do
-    namespace :v1 do
-      get 'rooms' => 'rooms#index'
-    end
-  end
-
   scope "admin" do
     get 'rooms/sort' => "rooms#index_sort", :as => "sort_rooms"
     put 'rooms/sort' => "rooms#update_sort"
-    delete 'blocks/destroy/:id' => "blocks#destroy", :as => :destroy_block
-    post 'blocks/destroy_existing_reservations' => "blocks#destroy_existing_reservations", :as => 'destroy_existing_reservations'
-    get 'blocks/index_existing_reservations' => "blocks#index_existing_reservations", :as => 'index_existing_reservations'
+    match 'blocks/destroy/:id' => "blocks#destroy", :as => "destroy_block"
+    match 'blocks/destroy_existing_reservations' => "blocks#destroy_existing_reservations", :as => 'destroy_existing_reservations'
+    match 'blocks/index_existing_reservations' => "blocks#index_existing_reservations", :as => 'index_existing_reservations'
 
-    resources :user_sessions
-    resources :users
+    # resources :users do
+      resources :users
+    # end
     resources :rooms
     resources :blocks
     resources :reports
     resources :room_groups
 
-    match 'reports' => "reservations#generate_report", via: [:get, :post]
+    match 'reports' => "reservations#generate_report"
   end
   get 'rooms/:id' => "rooms#show", :as => "show_room_details"
 
-  match 'reservations/resend_email' => "reservations#resend_email", via: [:get, :post]
+  match 'reservations/resend_email' => "reservations#resend_email"
   resources :reservations do
     put "delete" => "reservations#delete"
   end
 
-  match 'login', :to => 'user_sessions#new', :as => :login, via: [:get, :post]
-  match 'logout', :to => 'user_sessions#destroy', :as => :logout, via: [:get, :post]
-  match 'validate', :to => 'user_sessions#validate', :as => :validate, via: [:get, :post]
 
-  match 'admin' => "rooms#index", via: [:get, :post]
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
+  devise_scope :user do
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+    get 'login/', to: redirect('/users/auth/nyulibraries'), as: :login
+  end
+
+
+  match 'admin' => "rooms#index"
 
   root :to => "reservations#index"
 end

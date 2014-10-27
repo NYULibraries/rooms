1454a3fa8ee9ccc809d0e39e5d6dac80ac7f4407
hab278 <hab278@nyu.edu>
2014-10-27 10:56:04 -0400

    Remove auth_logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,12 +17,12 @@ class User < ActiveRecord::Base
   before_save :set_static_admins
 
   # Configure authlogic
-  acts_as_authentic do |c|
-    c.validations_scope = :username
-    c.validate_password_field = false
-    c.require_password_confirmation = false
-    c.disable_perishable_token_maintenance = true
-  end
+  # acts_as_authentic do |c|
+  #   c.validations_scope = :username
+  #   c.validate_password_field = false
+  #   c.require_password_confirmation = false
+  #   c.disable_perishable_token_maintenance = true
+  # end
 
   ##
   # Create a CSV format with comma DSL

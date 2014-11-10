e8f2d0a5bd3c7d3fb77e2ec42212b9ea5ab6bf7d
hab278 <hab278@nyu.edu>
2014-11-10 17:24:22 -0500

    Add oft used factories into factories files

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -26,6 +26,55 @@ FactoryGirl.define do
       end
     end
 
+    factory :hasnt_been_used_undergrad do
+      user_attributes do
+        {
+          nyuidn: "N0000001",
+          primary_institution: "INST01",
+          institutions: ["INST01"],
+          bor_status: "2",
+          aleph_permissions:  {}
+        }
+      end
+    end
+
+    factory :no_bookings_undergrad do
+      user_attributes do
+        {
+          nyuidn: "N0000001",
+          primary_institution: "INST01",
+          institutions: ["INST01"],
+          bor_status: "2",
+          aleph_permissions:  {}
+        }
+      end
+    end
+
+    factory :nonadmin do
+      admin_roles_mask nil
+      user_attributes do {
+          nyuidn: "N0000000",
+          primary_institution: "INST01",
+          institutions: ["INST01"],
+          bor_status: "52",
+          aleph_permissions: {}
+        }
+      end
+    end
+
+    factory :shanghai_undergraduate do
+      firstname "Shangri"
+      lastname "La"
+      user_attributes do
+        {
+          nyuidn: "N0000001",
+          primary_institution: "INST01",
+          institutions: ["INST01"],
+          bor_status: "0",
+          aleph_permissions: {}
+        }
+      end
+    end
     factory :admin do
       admin_roles_mask 1
       factory :ny_admin do

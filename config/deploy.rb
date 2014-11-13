80ed6320e2b62449b6d6fd76619d0422d48ebaae
B Alter <barnaby.alter@gmail.com>
2014-11-13 13:46:19 -0500

    Added a cap task so that the report caching happens on the server, not on jenkins where it was happening before

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,10 @@
 require 'formaggio/capistrano'
 set :app_title, "rooms"
 set :recipient, "rooms.admin@library.nyu.edu"
+
+namespace :reporting do
+  desc "Startup delayed jobs script"
+  task :cache do
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake cache:reporting"
+  end
+end

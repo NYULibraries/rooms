7719893a2b4ed62b1e90b8ce2264de7a6bca1605
hab278 <hab278@nyu.edu>
2014-12-04 20:20:10 -0500

    Define steps for feature

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,32 @@
+Around('@omniauth_test') do |scenario, block|
+  OmniAuth.config.test_mode = true
+  block.call
+  OmniAuth.config.test_mode = false
+end
+
+Given(/^I am logged in$/) do
+  OmniAuth.config.mock_auth[:nyulibraries] = omniauth_hash
+  visit '/login'
+end
+
+Given(/^I am not logged in$/) do
+  OmniAuth.config.mock_auth[:nyulibraries] = nil
+end
+
+Then(/^I should be redirected to login page$/) do
+  expect(page).to have_text 'Select your affiliation'
+end
+
+Then(/^I should see a logout link$/) do
+  expect(page).to have_css('.nyu-login i.icons-famfamfam-lock')
+  expect(page).to have_css('.nyu-login a.logout')
+end
+#
+Then(/^I should see "(.*?)" as the text of the logout link$/) do |text|
+  logout_link = find(:css, '.nyu-login a.logout')
+  expect(logout_link).to have_text text
+end
+
+Given(/^I am on the homepage$/) do
+  visit '/'
+end

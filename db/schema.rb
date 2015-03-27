8de375d020c6e36cf4e7aee7fa4ceebae1cbcc2e
hab278 <hab278@nyu.edu>
2015-03-27 17:21:08 -0400

    Update schema using latest syntax in Ruby and RoR

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,11 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131211223156) do
+ActiveRecord::Schema.define(version: 20141210191418) do
 
-  create_table "reservations", :force => true do |t|
+  create_table "reservations", force: true do |t|
     t.integer  "user_id"
     t.integer  "room_id"
     t.datetime "start_dt"
@@ -21,25 +21,25 @@ ActiveRecord::Schema.define(:version => 20131211223156) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title"
-    t.boolean  "is_block",            :default => false
+    t.boolean  "is_block",            default: false
     t.string   "deleted_by"
     t.string   "cc"
     t.string   "config_option"
-    t.boolean  "deleted",             :default => false
+    t.boolean  "deleted",             default: false
     t.string   "created_at_timezone"
     t.string   "deleted_at_timezone"
     t.datetime "deleted_at"
   end
 
-  create_table "room_groups", :force => true do |t|
+  create_table "room_groups", force: true do |t|
     t.string   "title"
     t.integer  "admin_roles_mask"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.string   "code"
   end
 
-  create_table "rooms", :force => true do |t|
+  create_table "rooms", force: true do |t|
     t.integer  "sort_order"
     t.string   "title"
     t.text     "description"
@@ -49,24 +49,24 @@ ActiveRecord::Schema.define(:version => 20131211223156) do
     t.string   "size_of_room"
     t.string   "image_link"
     t.text     "hours"
-    t.integer  "sort_size_of_room", :default => 0
+    t.integer  "sort_size_of_room", default: 0
     t.integer  "room_group_id"
     t.string   "opens_at"
     t.string   "closes_at"
     t.boolean  "collaborative"
   end
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "firstname"
     t.string   "lastname"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "username",          :default => "", :null => false
+    t.string   "username",          default: "", null: false
     t.string   "mobile_phone"
     t.string   "crypted_password"
     t.string   "password_salt"
-    t.string   "persistence_token",                 :null => false
+    t.string   "persistence_token",              null: false
     t.datetime "last_request_at"
     t.datetime "current_login_at"
     t.datetime "expiration_date"

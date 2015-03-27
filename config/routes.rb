80bee782fad73853529b95d3d0554852240ca3b9
hab278 <hab278@nyu.edu>
2015-03-27 17:10:49 -0400

    Update routes to reflect Rails 4 methodology

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rooms::Application.routes.draw do
   scope "admin" do
     get 'rooms/sort' => "rooms#index_sort", :as => "sort_rooms"
     put 'rooms/sort' => "rooms#update_sort"
-    match 'blocks/destroy/:id' => "blocks#destroy", :as => "destroy_block"
-    match 'blocks/destroy_existing_reservations' => "blocks#destroy_existing_reservations", :as => 'destroy_existing_reservations'
-    match 'blocks/index_existing_reservations' => "blocks#index_existing_reservations", :as => 'index_existing_reservations'
+    match 'blocks/destroy/:id' => "blocks#destroy", :as => "destroy_block", via: [:get, :post]
+    match 'blocks/destroy_existing_reservations' => "blocks#destroy_existing_reservations", :as => 'destroy_existing_reservations', via: [:get, :post]
+    match 'blocks/index_existing_reservations' => "blocks#index_existing_reservations", :as => 'index_existing_reservations', via: [:get, :post]
 
     resources :user_sessions
     resources :users
@@ -14,20 +14,20 @@ Rooms::Application.routes.draw do
     resources :reports
     resources :room_groups
 
-    match 'reports' => "reservations#generate_report"
+    match 'reports' => "reservations#generate_report", via: [:get, :post]
   end
   get 'rooms/:id' => "rooms#show", :as => "show_room_details"
 
-  match 'reservations/resend_email' => "reservations#resend_email"
+  match 'reservations/resend_email' => "reservations#resend_email", via: [:get, :post]
   resources :reservations do
     put "delete" => "reservations#delete"
   end
 
-  match 'login', :to => 'user_sessions#new', :as => :login
-  match 'logout', :to => 'user_sessions#destroy', :as => :logout
-  match 'validate', :to => 'user_sessions#validate', :as => :validate
+  match 'login', :to => 'user_sessions#new', :as => :login, via: [:get, :post]
+  match 'logout', :to => 'user_sessions#destroy', :as => :logout, via: [:get, :post]
+  match 'validate', :to => 'user_sessions#validate', :as => :validate, via: [:get, :post]
 
-  match 'admin' => "rooms#index"
+  match 'admin' => "rooms#index", via: [:get, :post]
 
   root :to => "reservations#index"
 end

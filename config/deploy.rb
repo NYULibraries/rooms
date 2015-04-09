3f34d676040c7f481b8f2ebb2a0dd39262f38f00
B Alter <barnaby.alter@gmail.com>
2015-04-09 11:47:19 -0400

    Force capistrano to use Ruby 2.1.5 on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 require 'formaggio/capistrano'
 set :app_title, "rooms"
 set :recipient, "rooms.admin@library.nyu.edu"
+# Run using ruby 2.1.5
+set :rvm_ruby_string, "ruby-2.1.5"
 
 namespace :reporting do
   desc "Startup delayed jobs script"

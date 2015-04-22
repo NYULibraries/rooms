02e34a9198ba1929a1f7050d4bc3b162d137d82c
B Alter <barnaby.alter@gmail.com>
2015-04-22 15:09:07 -0400

    Made user_attributes serializable when trying to migrate aleph_id

--- db/migrate/20141028203632_populate_aleph_id_from_user_attributes.rb
+++ db/migrate/20141028203632_populate_aleph_id_from_user_attributes.rb
@@ -1,6 +1,7 @@
 class PopulateAlephIdFromUserAttributes < ActiveRecord::Migration
   def up
     say_with_time "Migrating Aleph ID." do
+      User.class_eval { serialize :user_attributes }
       User.all.each do |user|
         user.update_attribute :aleph_id, user.user_attributes[:nyuidn]
       end

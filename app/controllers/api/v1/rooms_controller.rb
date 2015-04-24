058e50c858a7d1f768980e3f6899deefa7152882
B Alter <barnaby.alter@gmail.com>
2015-04-24 12:32:40 -0400

    Don't need to account for cancancan in api, because the default doesn't check for authorization

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -2,8 +2,6 @@ module Api
   module V1
     class RoomsController < ApplicationController
       protect_from_forgery with: :null_session
-
-      skip_authorization_check
       respond_to :json
 
       def index

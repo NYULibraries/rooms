0752f5079aa3d37af5309c32f0dd080a1412a788
hab278 <hab278@nyu.edu>
2015-09-10 14:35:36 -0400

    An artifact of merge, was testing the old way. Tests updated

--- test/functional/rooms_controller_test.rb
+++ test/functional/rooms_controller_test.rb
@@ -44,40 +44,38 @@ class RoomsControllerTest < ActionController::TestCase
   end
 
   test "create new room" do
-    VCR.use_cassette('create new room', match_requests_on: [:method, :uri_ignoring_trailing_id]) do
-      assert_difference('Room.count', 1) do
-        post(:create, :room => {
-                        :room_group_id => room_groups(:one).to_param,
-                        :title => "Cool new room",
-                        :type_of_room => "Graduate study",
-                        :collaborative => "1",
-                        :description => "A cooler, newer room",
-                        :size_of_room => "1 Person",
-                        :image_link => "http://localhost"
-                      }, :opens_at => {
-                        :hour => '7',
-                        :minute => '0',
-                        :ampm => 'am'
-                      }, :closes_at => {
-                        :hour => '7',
-                        :minute => '0',
-                        :ampm => 'am'
-                      }
-        )
-      end
-      assert assigns(:room)
-      assert_equal "Cool new room", assigns(:room).title
-      assert_equal "Graduate study", assigns(:room).type_of_room
-      assert_equal true, assigns(:room).collaborative
-      assert_equal "A cooler, newer room", assigns(:room).description
-      assert_equal "1 Person", assigns(:room).size_of_room
-      assert_equal "http://localhost", assigns(:room).image_link
-      assert_equal " 7:00", assigns(:room).opens_at
-      assert_equal " 7:00", assigns(:room).closes_at
-      assert_redirected_to room_url(assigns(:room))
-      assert_difference('Room.count', -1) do
-        delete :destroy, :id => assigns(:room).id
-      end
+    assert_difference('Room.count', 1) do
+      post(:create, :room => {
+                      :room_group_id => FactoryGirl.create(:room_group).to_param,
+                      :title => "Cool new room",
+                      :type_of_room => "Graduate study",
+                      :collaborative => "1",
+                      :description => "A cooler, newer room",
+                      :size_of_room => "1 Person",
+                      :image_link => "http://localhost"
+                    }, :opens_at => {
+                      :hour => '7',
+                      :minute => '0',
+                      :ampm => 'am'
+                    }, :closes_at => {
+                      :hour => '7',
+                      :minute => '0',
+                      :ampm => 'am'
+                    }
+      )
+    end
+    assert assigns(:room)
+    assert_equal "Cool new room", assigns(:room).title
+    assert_equal "Graduate study", assigns(:room).type_of_room
+    assert_equal true, assigns(:room).collaborative
+    assert_equal "A cooler, newer room", assigns(:room).description
+    assert_equal "1 Person", assigns(:room).size_of_room
+    assert_equal "http://localhost", assigns(:room).image_link
+    assert_equal " 7:00", assigns(:room).opens_at
+    assert_equal " 7:00", assigns(:room).closes_at
+    assert_redirected_to room_url(assigns(:room))
+    assert_difference('Room.count', -1) do
+      delete :destroy, :id => assigns(:room).id
     end
   end
 
@@ -88,36 +86,35 @@ class RoomsControllerTest < ActionController::TestCase
   end
 
   test "update room" do
-    VCR.use_cassette('update existing room') do
-      put :update, :id => rooms(:individual), :room => {
-                                                  :title => "Changing Titles",
-                                                  :type_of_room => "Changing Type",
-                                                  :collaborative => "0",
-                                                  :description => "Changing description",
-                                                  :size_of_room => "changing size",
-                                                  :image_link => "http://localhost/changedsize"
-                                              }, :opens_at => {
-                                                  :hour => '7',
-                                                  :minute => '0',
-                                                  :ampm => 'am'
-                                              }, :closes_at => {
-                                                  :hour => '7',
-                                                  :minute => '0',
-                                                  :ampm => 'am'
-                                              }
+    room = FactoryGirl.create(:room)
+    put :update, :id => room.id, :room => {
+                                                :title => "Changing Titles",
+                                                :type_of_room => "Changing Type",
+                                                :collaborative => "0",
+                                                :description => "Changing description",
+                                                :size_of_room => "changing size",
+                                                :image_link => "http://localhost/changedsize"
+                                            }, :opens_at => {
+                                                :hour => '7',
+                                                :minute => '0',
+                                                :ampm => 'am'
+                                            }, :closes_at => {
+                                                :hour => '7',
+                                                :minute => '0',
+                                                :ampm => 'am'
+                                            }
 
-      assert_equal flash[:notice], I18n.t("rooms.update.success")
-      room = Room.find(rooms(:individual))
-      assert_equal "Changing Titles", room.title
-      assert_equal "Changing Type", room.type_of_room
-      assert_equal false, room.collaborative
-      assert_equal "Changing description", room.description
-      assert_equal "changing size", room.size_of_room
-      assert_equal "http://localhost/changedsize", room.image_link
-      assert_equal " 7:00", room.opens_at
-      assert_equal " 7:00", room.closes_at
-      assert_redirected_to room_path(rooms(:individual))
-    end
+    assert_equal flash[:notice], I18n.t("rooms.update.success")
+    room = Room.find(room)
+    assert_equal "Changing Titles", room.title
+    assert_equal "Changing Type", room.type_of_room
+    assert_equal false, room.collaborative
+    assert_equal "Changing description", room.description
+    assert_equal "changing size", room.size_of_room
+    assert_equal "http://localhost/changedsize", room.image_link
+    assert_equal " 7:00", room.opens_at
+    assert_equal " 7:00", room.closes_at
+    assert_redirected_to room_path(room)
   end
 
   test "deletes room" do

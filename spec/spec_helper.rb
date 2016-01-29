df523825eba214a4146b31f90b789142afcc9abe
barnabyalter <barnaby.alter@nyu.edu>
2016-01-29 16:50:56 -0500

    Changed stub syntax and removed autorun from spec helper to stop deprecation warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,8 +15,6 @@ if ENV["RAILS_ENV"] == "test"
   require File.expand_path('../../config/environment', __FILE__)
 end
 require 'rspec/rails'
-require 'rspec/autorun'
-
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.

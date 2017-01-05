8dd95a2b7b4bcb5f7a62f32338a383f79c83c8eb
Barnaby Alter <barnaby.alter@nyu.edu>
2017-01-05 15:28:47 -0500

    A more interesting query to ES

--- es.sh
+++ es.sh
@@ -1,18 +1,46 @@
-curl -XGET 'http://localhost:9200/test_reservations/_search?pretty' -d '
+curl -XGET 'http://localhost:9200/staging_reservations/_search?pretty' -d '
 {
   "query": {
     "constant_score": {
       "filter": {
         "bool": {
           "must": [
+            { "terms": { "room_id": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] } },
             { "term": { "deleted": false } },
-            { "term": { "is_block": false } },
-            { "term": { "user_id": 482 } },
-            { "term": { "start_day": "2016-12-22" }}
+            { "bool": {
+              "should": [
+                {
+                  "bool": {
+                    "must": [
+                      { "range": { "start_dt": { "gte": "2017-01-05T13:30:00+00:00" } } },
+                      { "range": { "start_dt": { "lt": "2017-01-05T16:00:00+00:00" } } }
+                    ]
+                  }
+                },
+                {
+                  "bool": {
+                    "must": [
+                      { "range": { "end_dt": { "gt": "2017-01-05T13:30:00+00:00" } } },
+                      { "range": { "end_dt": { "lte": "2017-01-05T16:00:00+00:00" } } }
+                    ]
+                  }
+                },
+                {
+                  "bool": {
+                    "must": [
+                      { "range": { "start_dt": { "lte": "2017-01-05T13:30:00+00:00" } } },
+                      { "range": { "end_dt": { "gte": "2017-01-05T16:00:00+00:00" } } }
+                    ]
+                  }
+                }
+              ]
+            }
+            }
           ]
         }
       }
     }
-  }
+  },
+  "size": 200
 }
 '

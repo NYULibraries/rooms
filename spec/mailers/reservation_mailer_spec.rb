4aba003cece311514cd88d5a2235a7e6497f8926
Barnaby Alter <barnaby.alter@nyu.edu>
2017-09-14 16:56:14 -0400

    Change cancel or adjust reservation link in email to point to this app

--- spec/mailers/reservation_mailer_spec.rb
+++ spec/mailers/reservation_mailer_spec.rb
@@ -23,7 +23,7 @@ describe ReservationMailer, elasticsearch: true do
     it { is_expected.to include "Time: #{reservation.start_dt.strftime('%I:%M %p')} - #{reservation.end_dt.strftime('%I:%M %p')}" }
     it { is_expected.to include "Location: #{reservation.room.title}" }
     it { is_expected.to include "If you need help finding your study room or need to report a problem, visit the Circulation Services Desk (on the 1st floor of Bobst Library) or use <a href=\"http://library.nyu.edu/ask\">Ask a Librarian</a>" }
-    it { is_expected.to include "To adjust or cancel your reservation, <a href=\"https://library.nyu.edu/services/study-spaces/reservable-study-spaces/reservable-study-rooms-bobst-library/\">https://library.nyu.edu/services/study-spaces/reservable-study-spaces/reservable-study-rooms-bobst-library/</a>" }
+    it { is_expected.to include "To adjust or cancel your reservation, <a href=\"https://rooms.library.nyu.edu/\">https://rooms.library.nyu.edu/</a>" }
     it { is_expected.to include "Please help us improve this service by taking <a href=\"https://nyu.qualtrics.com/jfe/form/SV_3fTab30NE6XdnsF\">this brief survey</a>" }
   end
 
@@ -35,7 +35,7 @@ describe ReservationMailer, elasticsearch: true do
     it { is_expected.to include "Time: #{reservation.start_dt.strftime('%I:%M %p')} - #{reservation.end_dt.strftime('%I:%M %p')}" }
     it { is_expected.to include "Location: #{reservation.room.title}" }
     it { is_expected.to include "If you need help finding your study room or need to report a problem, visit the Circulation Services Desk (on the 1st floor of Bobst Library) or use <a href=\"http://library.nyu.edu/ask\">Ask a Librarian</a>" }
-    it { is_expected.to include "To adjust or cancel your reservation, <a href=\"https://library.nyu.edu/services/study-spaces/reservable-study-spaces/reservable-study-rooms-bobst-library/\">https://library.nyu.edu/services/study-spaces/reservable-study-spaces/reservable-study-rooms-bobst-library/</a>" }
+    it { is_expected.to include "To adjust or cancel your reservation, <a href=\"https://rooms.library.nyu.edu\">https://rooms.library.nyu.edu/</a>" }
     it { is_expected.to include "Please help us improve this service by taking <a href=\"https://nyu.qualtrics.com/jfe/form/SV_3fTab30NE6XdnsF\">this brief survey</a>" }
   end
 

585bfba2082a34b56c3638ec03b9b4ea1a57f641
Barnaby Alter <barnaby.alter@nyu.edu>
2017-09-18 15:11:15 -0400

    First attempt at adding docker

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -2,5 +2,9 @@ require 'elasticsearch/model'
 require 'elasticsearch/transport'
 
 unless Rails.env.test? || Rails.env.development?
-  Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['ROOMS_BONSAI_URL']
+  Elasticsearch::Model.client = Elasticsearch::Client.new(url: ENV['ROOMS_BONSAI_URL'])
+else
+  if ENV['ROOMS_DEV_ELASTICSEARCH_URL']
+    Elasticsearch::Model.client = Elasticsearch::Client.new(url: ENV['ROOMS_DEV_ELASTICSEARCH_URL'])
+  end
 end

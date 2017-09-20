49ccbeb5b6a664cf53ede428fa79e29ceb47a78c
Barnaby Alter <barnaby.alter@nyu.edu>
2017-09-20 12:11:47 -0400

    Minify js and css in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,9 @@ Rooms::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
